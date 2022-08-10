e66f2baa823a98bb4e0af331491743d4151c5055
revazashvili <levanrevazashvili@gmail.com>
2022-08-10 21:08:12 +0400

    remove some packages

--- csharp/src/Program.cs
+++ csharp/src/Program.cs
@@ -4,17 +4,10 @@ builder.Services.AddHealthChecks();
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.MapHealthChecks("/healthz");
-
-// Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
