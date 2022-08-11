a8c73e3ffe6fcf6e144dcbb18952d614344c2c44
revazashvili <levanrevazashvili@gmail.com>
2022-08-11 21:37:33 +0400

    read weathers from postgres

--- csharp/src/Controllers/WeatherForecastController.cs
+++ csharp/src/Controllers/WeatherForecastController.cs
@@ -1,30 +1,68 @@
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 
 namespace api.Controllers;
 
 [ApiController]
-[Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static string[] _summaries = {};
+    private readonly string _postgresConnection;
+    private const string DbName = "Weather";
+    private const string DropDbCommand = $"DROP DATABASE IF EXISTS {DbName};";
+    private const string CreateDbCommand = $"CREATE DATABASE {DbName};";
+    private const string CreateTableCommand = @"CREATE TABLE IF NOT EXISTS Weathers (
+              Date TIMESTAMP not null,  
+              TemperatureC integer NOT NULL,
+              Summary varchar(450) NOT NULL
+            )";
+
+    private const string InsertCommand = "INSERT INTO Weathers(Date,TemperatureC,Summary) VALUES (@Date,@TemperatureC,@Summary);";
     
     public WeatherForecastController(IConfiguration configuration)
     {
         _summaries = configuration.GetSection("Summaries").Get<string[]>();
-
-        var mongoConnectionString = configuration.GetConnectionString("MongoConnection");
-        Console.WriteLine($"Mongo Connection: {mongoConnectionString}");
+        _postgresConnection = configuration.GetConnectionString("Postgres");
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet("/weathers")]
+    public IEnumerable<WeatherForecast> Weathers()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        using var connection = new NpgsqlConnection(_postgresConnection);
+        return connection.Query<WeatherForecast>("select * from Weathers");
+    }
+
+    [HttpGet("/seed")]
+    public IActionResult Seed()
+    {
+        using var connection = new NpgsqlConnection(_postgresConnection);
+        var weathers = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = _summaries[Random.Shared.Next(_summaries.Length)]
+            })
+            .ToList();
+        weathers.ForEach(weather =>
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = _summaries[Random.Shared.Next(_summaries.Length)]
-        })
-        .ToArray();
+            connection.Execute(InsertCommand, new
+            {
+                weather.Date,
+                weather.TemperatureC,
+                weather.Summary
+            });
+        });
+        return Ok();
+    }
+    
+    [HttpGet("/setup")]
+    public IActionResult CreateDatabaseAndTable()
+    {
+        using var connection = new NpgsqlConnection(_postgresConnection);
+        connection.Execute(DropDbCommand);
+        connection.Execute(CreateDbCommand);
+        connection.Execute(CreateTableCommand);
+        return Ok();
     }
 }
